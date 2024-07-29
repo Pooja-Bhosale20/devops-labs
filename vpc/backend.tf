987333324f4b894e4bbf7c7deb588147b59fc62a
Pooja-Bhosale20 <bhosale.pooja028@gmail.com>
2024-07-29 12:44:52 +0530

    Delete vpc directory

--- vpc/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "devopslabs706"
-    key    = "devops-labs/vpc/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- vpc/vpc.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_vpc" "app_vpc" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name = local.vpc_name
-  }
-}
