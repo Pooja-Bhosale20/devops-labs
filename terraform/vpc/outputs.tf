935b88b76cd96d4c19135d6ba6c8c41577d46d04
Pooja-Bhosale20 <bhosale.pooja028@gmail.com>
2024-07-29 12:38:26 +0530

    Renaming folder names (#2)
    * VPC Terraform code
    
    * Renaming folder name

---
+++ terraform/vpc/outputs.tf
@@ -0,0 +1,12 @@
+output "vpc_id" {
+  value = aws_vpc.app_vpc.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public_subnet[*].id
+  
+}
+
+output "private_subnets" {
+  value = aws_subnet.private_subnet[*].id
+}
